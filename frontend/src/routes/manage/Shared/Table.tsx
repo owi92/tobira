e9ae10b76ba8e3028d5e737878dc1c819a02162f
Ole Wieners <olewieners@yahoo.com>
2025-02-26 17:58:44 +0100

    Add notification for series creation
    This utilizes the recently added notification context
    to show a note "Series has been created" on the
    series details page. The user will be redirected to that
    page after a series has been freshly created.
    The note will clear when the user navigates away from
    that page.

--- frontend/src/routes/manage/Shared/Table.tsx
+++ frontend/src/routes/manage/Shared/Table.tsx
@@ -19,7 +19,7 @@ import { ManageRoute } from "..";
 import { COLORS } from "../../../color";
 import { PageTitle } from "../../../layout/header/ui";
 import { Breadcrumbs } from "../../../ui/Breadcrumbs";
-import { Link, useRouter } from "../../../router";
+import { Link } from "../../../router";
 import { VideosSortColumn } from "../Video/__generated__/VideoManageQuery.graphql";
 import { SeriesSortColumn } from "../Series/__generated__/SeriesManageQuery.graphql";
 import { useNotification } from "../../../ui/NotificationContext";
